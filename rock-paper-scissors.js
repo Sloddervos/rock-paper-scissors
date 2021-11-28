60ba198a41c1c0c24540aa9aa8379b3e0f39d9a6
Oscar <oscar.bailey95@gmail.com>
2021-11-28 11:39:17 +0000

    Rock paper scissors reworked version with UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,19 +9,21 @@ function computerPlay(){
 
 function singleRound(playerSelection, computerSelection){
 computerSelection = computerPlay();
-playerSelection = prompt("make your choice");
-
+resultsDiv.textContent = '';
+if (playerSelection === 0){
+    playerSelection = prompt("make your choice");
+}
 
 
 if (playerSelection === null){
-    console.log('game cancelled');
+    resultsDiv.textContent = 'game cancelled';
     return;
 }
 
 playerSelection = playerSelection.toLowerCase();
 
 while (playerSelection !== 'rock' && playerSelection !== 'scissors' && playerSelection !== 'paper') {
-    console.log();
+    resultsDiv.textContent = '';
     playerSelection = prompt('not an acceptable string, try again');
     playerSelection = playerSelection.toLowerCase();
 }
@@ -30,55 +32,110 @@ while (playerSelection !== 'rock' && playerSelection !== 'scissors' && playerSel
 
 if (playerSelection === computerSelection)
     {
-    console.log('Tie, ' + computerSelection + ' ties ' + playerSelection);
+    resultsDiv.textContent = ('Tie, ' + computerSelection + ' ties ' + playerSelection);
     return 3; 
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock')
     {
-        console.log ('You win! ' + playerSelection + ' beats ' + computerSelection);
+        resultsDiv.textContent =  ('You win! ' + playerSelection + ' beats ' + computerSelection);
         return 1;
     }
     else
     {
-        console.log('You lose! ' + computerSelection + ' beats ' + playerSelection);
+        resultsDiv.textContent = ('You lose! ' + computerSelection + ' beats ' + playerSelection);
         return 2;
     }
 
 
 }
+// logic to play 5 rounds and keep track of score
+let x = 0;
+let y = 0;
 
+function game(value){
+   
 
-function game(){
-    let x = 0;
-    let y = 0;
-
-    for (let i = 0; i < 5; i++) {
-
-        var result = singleRound();
+    
+        var result = singleRound(value);
        if (result === 1){
            x++;
-           console.log(x + ' - ' + y);
+           resultsDiv.textContent += (' ' +x + ' - ' + y)
        }
        else if (result === 2){
            y++;    
-           console.log(x + ' - ' + y);
+           resultsDiv.textContent += (' ' + x + ' - ' + y)
        }
        else if (result === 3){
            x = x + .5;
            y = y + .5;
-           console.log(x + ' - ' + y);
-
-       }
-       else {
-           i = 5;
-           
-       }
-       
+           resultsDiv.textContent += (' ' + x + ' - ' + y)
+ }
+ 
+ if (x + y === 5) {
+    resultsDiv.textContent += (' Final result is ' + x + ' - ' + y)
+    x = 0;
+    y = 0;
+    }
       
       
 
-       
+}
 
-}
-return 'final result is ' + x + ' - ' + y;  
-}
+
+
+
+
+
+// this is to create a div
+const body = document.body
+const div = document.createElement('div')
+// Underneath here I'm adding buttons
+const buttonOne = document.createElement('button')
+const buttonTwo = document.createElement('button')
+const buttonThree = document.createElement('button') 
+
+// Creating another div to display results
+const resultsDiv = document.createElement('div')
+
+
+// here I add two class attributes, one to style the buttons and the resultsdiv 
+//  and another to style the container div 
+buttonOne.setAttribute('class', 'buttonStyles')
+buttonTwo.setAttribute('class', 'buttonStyles')
+buttonThree.setAttribute('class', 'buttonStyles')
+resultsDiv.setAttribute('class', 'buttonStyles')
+resultsDiv.setAttribute('id', 'resultsDivStyles')
+div.setAttribute('class', 'container')
+
+
+// here I want to add textcontent to my buttons
+buttonOne.textContent = 'rock'
+buttonTwo.textContent = 'paper'
+buttonThree.textContent = 'scissors'
+
+// here I'm appending the newly created elements to the body and div respectively
+body.append(div)
+div.append(buttonOne, buttonTwo, buttonThree, resultsDiv)
+
+
+// here i want to add an event listener for when the buttons are clicked, need to make it
+// so that each button has a unique eventlistener that sends the corresponding text value
+// through to the function
+
+buttonOne.addEventListener('click', () => {
+  singleRound('rock');
+  game('rock');
+});
+
+buttonTwo.addEventListener('click', () => {
+    singleRound('paper');
+    game('paper');
+  });
+  buttonThree.addEventListener('click', () => {
+    singleRound('scissors');
+    game('paper');
+  });
+    
+
+
+
